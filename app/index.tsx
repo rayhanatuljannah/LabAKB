f85b3cd6da6a10b76e925c7cf1acea21cc351d17
rayhanatuljannah <105841110822@student.unismuh.ac.id>
2025-07-14 22:06:25 +0700

    update project

--- app/index.tsx
+++ app/index.tsx
@@ -9,15 +9,15 @@ import {
 } from 'react-native';
 
 const imageData = [
-  { id: 1, mainSrc: 'https://picsum.photos/id/10/200', altSrc: 'https://picsum.photos/id/11/200' },
-  { id: 2, mainSrc: 'https://picsum.photos/id/12/200', altSrc: 'https://picsum.photos/id/13/200' },
-  { id: 3, mainSrc: 'https://picsum.photos/id/14/200', altSrc: 'https://picsum.photos/id/15/200' },
-  { id: 4, mainSrc: 'https://picsum.photos/id/16/200', altSrc: 'https://picsum.photos/id/17/200' },
-  { id: 5, mainSrc: 'https://picsum.photos/id/18/200', altSrc: 'https://picsum.photos/id/19/200' },
-  { id: 6, mainSrc: 'https://picsum.photos/id/20/200', altSrc: 'https://picsum.photos/id/21/200' },
-  { id: 7, mainSrc: 'https://picsum.photos/id/22/200', altSrc: 'https://picsum.photos/id/23/200' },
-  { id: 8, mainSrc: 'https://picsum.photos/id/24/200', altSrc: 'https://picsum.photos/id/25/200' },
-  { id: 9, mainSrc: 'https://picsum.photos/id/26/200', altSrc: 'https://picsum.photos/id/27/200' },
+  { id: 1, mainSrc: 'https://picsum.photos/id/30/200', altSrc: 'https://picsum.photos/id/31/200' },
+  { id: 2, mainSrc: 'https://picsum.photos/id/32/200', altSrc: 'https://picsum.photos/id/33/200' },
+  { id: 3, mainSrc: 'https://picsum.photos/id/34/200', altSrc: 'https://picsum.photos/id/35/200' },
+  { id: 4, mainSrc: 'https://picsum.photos/id/36/200', altSrc: 'https://picsum.photos/id/37/200' },
+  { id: 5, mainSrc: 'https://picsum.photos/id/38/200', altSrc: 'https://picsum.photos/id/39/200' },
+  { id: 6, mainSrc: 'https://picsum.photos/id/40/200', altSrc: 'https://picsum.photos/id/41/200' },
+  { id: 7, mainSrc: 'https://picsum.photos/id/42/200', altSrc: 'https://picsum.photos/id/43/200' },
+  { id: 8, mainSrc: 'https://picsum.photos/id/44/200', altSrc: 'https://picsum.photos/id/45/200' },
+  { id: 9, mainSrc: 'https://picsum.photos/id/46/200', altSrc: 'https://picsum.photos/id/47/200' },
 ];
 
 export default function ImageGrid() {
@@ -56,7 +56,10 @@ export default function ImageGrid() {
     <ScrollView contentContainerStyle={styles.container}>
       <View style={styles.grid}>
         {images.map(img => (
-          <TouchableWithoutFeedback key={img.id} onPress={() => handlePress(img.id)}>
+          <TouchableWithoutFeedback
+            key={img.id}
+            onPress={() => handlePress(img.id)}
+          >
             <View style={styles.cell}>
               <Animated.Image
                 source={{ uri: img.isFlipped ? img.altSrc : img.mainSrc }}
@@ -91,10 +94,11 @@ const styles = StyleSheet.create({
     margin: 5,
     backgroundColor: '#eee',
     borderRadius: 8,
-    overflow: 'hidden', 
+    overflow: 'hidden',
   },
   image: {
-    width: '100%',
-    height: '100%',
+    width: 100,
+    height: 100,
+    borderRadius: 8,
   },
 });
